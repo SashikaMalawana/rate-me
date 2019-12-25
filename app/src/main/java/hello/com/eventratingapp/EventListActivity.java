030ed11b38ccf784aa42f545797784e8af6959c6
SashikaMalawana <Sashika.Irosha@gmail.com>
2019-12-25 12:21:17 +0530

    Code neatness

--- app/src/main/java/hello/com/eventratingapp/EventListActivity.java
+++ app/src/main/java/hello/com/eventratingapp/EventListActivity.java
@@ -12,13 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.ArrayList;
 
 public class EventListActivity extends AppCompatActivity {
