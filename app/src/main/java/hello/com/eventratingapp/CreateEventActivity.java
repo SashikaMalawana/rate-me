3e11cfffcd7f485129aecc622c87384e3ea1eaf9
SashikaMalawana <Sashika.Irosha@gmail.com>
2019-12-31 08:42:13 +0530

    Basic image uploading mechanism

--- app/src/main/java/hello/com/eventratingapp/CreateEventActivity.java
+++ app/src/main/java/hello/com/eventratingapp/CreateEventActivity.java
@@ -1,5 +1,8 @@
 package hello.com.eventratingapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,9 +11,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class CreateEventActivity extends AppCompatActivity {
 
@@ -22,6 +29,11 @@ public class CreateEventActivity extends AppCompatActivity {
     private EditText countryOfOriginEditText;
     private EditText originalLaguageEditTExt;
     private EditText genreEditText;
+
+    private StorageReference mStorage;
+    private Button mImageSelect;
+    private static final int GALLERY_INTENT = 2;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,10 @@ public class CreateEventActivity extends AppCompatActivity {
         countryOfOriginEditText = (EditText) findViewById(R.id.countryOfOriginField);
         originalLaguageEditTExt = (EditText) findViewById(R.id.originalLanguageField);
         genreEditText = (EditText) findViewById(R.id.genreField);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mImageSelect = (Button) findViewById(R.id.selectImageButton);
+        mProgressDialog = new ProgressDialog(this);
 
         storeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,5 +116,38 @@ public class CreateEventActivity extends AppCompatActivity {
 
             }
         });
+
+        mImageSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+
+            mProgressDialog.setMessage("Uploading...");
+            mProgressDialog.show();
+
+            Uri uri = data.getData();
+            StorageReference filepath = mStorage.child("photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(CreateEventActivity.this, "Upload done", Toast.LENGTH_LONG).show();
+                    mProgressDialog.dismiss();
+                }
+            });
+
+        }
+
     }
 }
